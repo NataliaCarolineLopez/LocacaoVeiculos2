7b5a8b7d421f6869bde3e3f478d872cbaffe85c5
NataliaCarolineLopez <nataliacarolinesenai@gmail.com>
2025-04-28 11:25:47 -0300

    arquivo veiculo

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'locadora/veiculos',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e64d962abcd7e9d9fff249204e618eca628d76f2',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'locadora/veiculos' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e64d962abcd7e9d9fff249204e618eca628d76f2',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
